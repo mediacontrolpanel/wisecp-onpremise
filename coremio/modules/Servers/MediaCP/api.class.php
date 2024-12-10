10faf46c130f4569671f3676133ef432be8e7fcd
Matthew Lear <matthew.lear@mediacp.net>
2024-12-10 20:49:14 +1100

    Improved logging

--- coremio/modules/Servers/MediaCP/api.class.php
+++ coremio/modules/Servers/MediaCP/api.class.php
@@ -39,12 +39,12 @@
 
             $url = ($this->server['secure'] == 1 ? 'https' : 'http') . '://' . $this->server["ip"] .':'. $this->server['port'] . $path;
 
-            if ( !$method ) $url .= '?'.http_build_query($data);
+            if ( !$method && is_array($data) && count($data) > 0) $url .= '?'.http_build_query($data);
             curl_setopt($ch, CURLOPT_URL, $url);
 
             $responseData = curl_exec($ch);
             $this->response = $responseData;
-            Modules::save_log("Servers","MediaCP",$url." - ".$path,$data,$responseData);
+            Modules::save_log("Servers","MediaCP",$url,$data,$responseData);
 
             if ( curl_getinfo($ch, CURLINFO_HTTP_CODE) == 401 ){
                 $this->error = "Unauthorized.<br /><br />The provided API key for the server is not valid.<br /><br />Refer to <a href='https://www.mediacp.net/doc/admin-server-manual/billing/clientexec-integration-guide/'>module documentation</a> for more information.";
@@ -55,7 +55,7 @@
             $response = json_decode($responseData);
 
             if ($responseData === false || (curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200 && $catchHttpCodeErrors)) {
-                if ( $response->errors ){
+                if ( $response && isset($response->errors) ){
                     $this->error = "MediaCP API Error:\nCall: {$path}\n";
                     foreach($response->errors as $message)
                         $this->error .= "{$message}\n";
@@ -63,7 +63,7 @@
                     $this->error = "MediaCP API Request / cURL Error: " . curl_getinfo($ch, CURLINFO_HTTP_CODE) . ' - '.curl_error($ch) . '<br /><br />' . print_r($response ?: $data, true) . "<br /><br />Refer to <a href='https://www.mediacp.net/doc/admin-server-manual/billing/wisecp-integration-guide/'>module documentation</a> for more information.";
                 }
 
-                Modules::save_log("Servers","MediaCP",$url." - ".$path,$data,$this->error);
+                Modules::save_log("Servers","MediaCP",$url,$data,$this->error);
                 return false;
             }
             curl_close($ch);
